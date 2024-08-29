31ceb30d533cf0cb36a1ae5394bcafd8d7e7908f
massone99 <lorenzomassone@gmail.com>
2024-08-29 18:36:50 +0200

    feat: added basic usage of status

--- todo_app/src/App.jsx
+++ todo_app/src/App.jsx
@@ -1,12 +1,38 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function App() {
+    const [newItem, setNewItem] =  useState("")
     // class is a reserved keyword in React
-    return <form className="new-item-form">
-        <div className="form-row">
-        {/* Same logic for the for keyword */}
-            <label htmlFor="item">New item</label>
-            <input type="text" id="item"></input>
-        </div>
-    </form>
+    return ( 
+        <>
+            <form className="new-item-form">
+                <div className="form-row">
+                    {/* Same logic for the for keyword */}
+                    <label htmlFor="item">New item</label>
+                    <input value={newItem} 
+                        onChange={e => setNewItem(e.target.value)}
+                        type="text" id="item"></input>
+                    <button className="btn">Add</button>
+                </div>
+            </form>
+            <h1>Todo List</h1>
+            <ul className="list">
+                <li>
+                    <label>
+                        <input type="checkbox"/>
+                        Item 1
+                    </label>
+                    <button className="btn btn-danger">Delete</button>
+                </li>
+                <li>
+                    <label>
+                        <input type="checkbox"/>
+                        Item 2
+                    </label>
+                    <button className="btn btn-danger">Delete</button>
+                </li>
+            </ul>
+        </>
+    )
 }
